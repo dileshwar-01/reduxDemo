66aa49504d66502fb602803cc8f3586566777319
dileshwar-01 <rajputdileshwar16@gmail.com>
2025-06-08 12:48:48 +0530

    Final commit

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { actionCreators } from '../state'
 
 const Shop = () => {
+    const dispatch = useDispatch()
+    const {withdrawMoney, depositMoney} =bindActionCreators(actionCreators,dispatch)
   return (
     <div>
         <h2>Deposit/Withdraw Money</h2>
-      <button className='btn btn-primary mx-2'>-</button>
+      <button onClick={()=>{withdrawMoney(100)}} className='btn btn-primary mx-2'>-</button>
       Update Balance
-      <button className='btn btn-primary mx-2'>+</button>
+      <button onClick={()=>{depositMoney(100)}} className='btn btn-primary mx-2'>+</button>
     </div>
   )
 }
